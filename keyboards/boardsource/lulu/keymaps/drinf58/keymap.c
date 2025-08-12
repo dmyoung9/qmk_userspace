389dfa04c8f127c7a1b612035084997b39dc9d10
David Young <davidyoung@planetbids.com>
2025-08-12 11:37:55 -0700

    Change menu to caps word

--- keyboards/boardsource/lulu/keymaps/drinf58/keymap.c
+++ keyboards/boardsource/lulu/keymaps/drinf58/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	  KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5,                      KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS,
 	  KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T,                      KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_EQL ,
 	  ADJUST , MOD_HLG, MOD_HLA, MOD_HLS, MOD_HLC, KC_G,                      KC_H   , MOD_HRC, MOD_HRS, MOD_HRA, MOD_HRG, KC_QUOT,
-	  KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B,    KC_ESC , KC_MUTE, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_BSLS,
+	  CW_TOGG, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B,    KC_ESC , KC_MUTE, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_BSLS,
 								 KC_LGUI, LOWER  , KC_DEL , KC_BSPC, KC_SPC , KC_ENT , RAISE  , QK_MACRO_0
 	),
 
