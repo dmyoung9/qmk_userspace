52b58773d9c676550914c0d6c48ccbc3eedaf1ce
David Young <davidyoung@planetbids.com>
2025-10-01 19:25:39 -0700

    Add parentheses/brackets/braces

--- keyboards/boardsource/lulu/keymaps/kbdd/keymap.c
+++ keyboards/boardsource/lulu/keymaps/kbdd/keymap.c
@@ -51,9 +51,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_NUM] = LAYOUT(
     _______, _______, _______, _______, _______, _______,                   XXXXXXX, XXXXXXX, KC_PSLS, KC_PAST, XXXXXXX, XXXXXXX,
-    _______, _______, _______, G_EMOJI, _______, _______,                   KC_PMNS, KC_P7  , KC_P8  , KC_P9  , XXXXXXX, XXXXXXX,
-    _______, _______, _______, _______, _______, _______,                   KC_PPLS, KC_P4  , KC_P5  , KC_P6  , XXXXXXX, XXXXXXX,
-    _______, _______, _______, _______, _______, _______, _______, CUS_TSK, KC_PDOT, KC_P1  , KC_P2  , KC_P3  , KC_BSLS, XXXXXXX,
+    _______, _______, _______, KC_LBRC, KC_RBRC, _______,                   KC_PMNS, KC_P7  , KC_P8  , KC_P9  , XXXXXXX, XXXXXXX,
+    _______, _______, _______, KC_LPRN, KC_RPRN, _______,                   KC_PPLS, KC_P4  , KC_P5  , KC_P6  , XXXXXXX, XXXXXXX,
+    _______, _______, _______, KC_LCBR, KC_RCBR, _______, G_EMOJI, CUS_TSK, KC_PDOT, KC_P1  , KC_P2  , KC_P3  , KC_BSLS, XXXXXXX,
                                _______, _______, _______, _______, KC_P0  , KC_EQL , _______, KC_CALC
 ),
 
