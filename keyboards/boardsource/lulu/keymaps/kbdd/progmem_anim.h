ccfd21516150c2706cef61a7801cd914d5d101e6
David Young <davidyoung@planetbids.com>
2026-04-19 11:48:28 -0700

    Update animation PROGMEMs with Cyber animations

--- keyboards/boardsource/lulu/keymaps/kbdd/progmem_anim.h
+++ keyboards/boardsource/lulu/keymaps/kbdd/progmem_anim.h
@@ -2,28 +2,21 @@
 
 #include QMK_KEYBOARD_H
 
-extern const uint8_t PROGMEM qwerty_0[], qwerty_1[], qwerty_2[], qwerty_3[], qwerty_4[], qwerty_5[];  // 72x12
-extern const uint8_t PROGMEM num_0[], num_1[], num_2[], num_3[], num_4[], num_5[];  // 72x12
-extern const uint8_t PROGMEM nav_0[], nav_1[], nav_2[], nav_3[], nav_4[], nav_5[], nav_6[], nav_7[], nav_8[], nav_9[];  // 72x12
-extern const uint8_t PROGMEM func_0[], func_1[], func_2[], func_3[], func_4[], func_5[], func_6[], func_7[];  // 72x12
-extern const uint8_t PROGMEM task_0[], task_1[], task_2[], task_3[];  // 72x12
-extern const uint8_t PROGMEM game_0[], game_1[], game_2[], game_3[], game_4[], game_5[];  // 72x12
-extern const uint8_t PROGMEM unicode_0[], unicode_1[], unicode_2[], unicode_3[], unicode_4[], unicode_5[], unicode_6[];  // 72x12
+extern const uint8_t PROGMEM boot_0[], boot_1[], boot_2[], boot_3[],
+                             boot_4[], boot_5[], boot_6[], boot_7[],
+                             boot_8[], boot_9[], boot_10[], boot_11[],
+                             boot_12[], boot_13[], boot_14[], boot_15[];
 
-extern const uint8_t PROGMEM wpm_frame[];  // 128x32
-extern const uint8_t PROGMEM logo[];  // 22x16
+extern const uint8_t PROGMEM qwerty_0[], qwerty_1[], qwerty_2[], qwerty_3[];
+extern const uint8_t PROGMEM symbol_0[], symbol_1[], symbol_2[], symbol_3[];
+extern const uint8_t PROGMEM navigation_0[], navigation_1[], navigation_2[], navigation_3[];
+extern const uint8_t PROGMEM function_0[], function_1[], function_2[], function_3[];
+extern const uint8_t PROGMEM gaming_0[], gaming_1[], gaming_2[], gaming_3[];
+extern const uint8_t PROGMEM unicode_0[], unicode_1[], unicode_2[], unicode_3[];
 
-extern const uint8_t PROGMEM layer_frame_0[], layer_frame_1[], layer_frame_2[], layer_frame_3[], layer_frame_4[];  // 86x16
-extern const uint8_t PROGMEM caps_frame_0[], caps_frame_1[], caps_frame_2[], caps_frame_3[];  // 41x16
-extern const uint8_t PROGMEM mods_frame_0[], mods_frame_1[], mods_frame_2[], mods_frame_3[], mods_frame_4[];  // 106x16
-
-extern const uint8_t PROGMEM caps_0[], caps_1[], caps_2[], caps_3[];  // 21x9
-extern const uint8_t PROGMEM super_0[], super_1[], super_2[], super_3[];  // 25x9
-extern const uint8_t PROGMEM alt_0[], alt_1[], alt_2[], alt_3[];  // 17x9
-extern const uint8_t PROGMEM shift_0[], shift_1[], shift_2[], shift_3[];  // 23x9
-extern const uint8_t PROGMEM ctrl_0[], ctrl_1[], ctrl_2[], ctrl_3[];  // 21x9
-
-extern const uint8_t PROGMEM wpm_frame_0[], wpm_frame_1[], wpm_frame_2[], wpm_frame_3[], wpm_frame_4[], wpm_frame_5[], wpm_frame_6[], wpm_frame_7[], wpm_frame_8[];
-extern const uint8_t PROGMEM wpm_0[], wpm_1[], wpm_2[];
+extern const uint8_t PROGMEM super_0[], super_1[], super_2[], super_3[];
+extern const uint8_t PROGMEM alt_0[], alt_1[], alt_2[], alt_3[];
+extern const uint8_t PROGMEM shift_0[], shift_1[], shift_2[], shift_3[];
+extern const uint8_t PROGMEM ctrl_0[], ctrl_1[], ctrl_2[], ctrl_3[];
 
 extern const uint8_t PROGMEM digit_0[], digit_1[], digit_2[], digit_3[], digit_4[], digit_5[], digit_6[], digit_7[], digit_8[], digit_9[];
